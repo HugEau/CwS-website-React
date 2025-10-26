d0e7ca211a09b6530bedfa6511e2837e73da6e60
HugEau <143726845+HugEau@users.noreply.github.com>
2025-10-26 15:45:26 +0100

    Update works data with new Pira Multisports images
    Added new images for Pira Multisports and updated works data. Removed Riding Cities and Print It entries.

--- src/assets/datas/works.js
+++ src/assets/datas/works.js
@@ -11,6 +11,11 @@ import valerieNextImg10 from '../images/ValerieBoutiqueNext/ValerieBoutiqueNext1
 import valerieNextImg11 from '../images/ValerieBoutiqueNext/ValerieBoutiqueNext11.webp'
 import valerieNextImg12 from '../images/ValerieBoutiqueNext/ValerieBoutiqueNext12.webp'
 
+import piraimg from '../images/PiraMultisports/piramultisport1.webp'
+import piraimg2 from '../images/PiraMultisports/piramultisport2.webp'
+import piraimg3 from '../images/PiraMultisports/piramultisport3.webp'
+import piraimg4 from '../images/PiraMultisports/piramultisport4.webp'
+
 import valerieImg from '../images/ValerieBoutique/valerieboutique.webp'
 import valerieImg2 from '../images/ValerieBoutique/valerieboutiqueVetement.webp'
 import valerieImg3 from '../images/ValerieBoutique/valerieboutiqueModal.webp'
@@ -24,9 +29,6 @@ import kasaImg2 from '../images/Kasa/kasaLogement.webp'
 import kasaImg3 from '../images/Kasa/kasaCollaps.webp'
 import kasaImg4 from '../images/Kasa/kasaAbout.webp'
 
-import ridingCitiesImg from '../images/RidingCities/ridingcities.webp'
-import ridingCitiesImg2 from '../images/RidingCities/ridingCitiesPlanning.webp'
-
 import CwSV1Img from '../images/CwSV1/cwsv1.webp'
 import CwSV1Img2 from '../images/CwSV1/cwsV1Portfolio.webp'
 import CwSV1Img3 from '../images/CwSV1/cwsV1Contact.webp'
@@ -38,9 +40,6 @@ import CwSReactImg4 from '../images/CwSReact/CwSReactWork.webp'
 
 import bookiImg from '../images/Booki/booki.webp'
 import bookiImg2 from '../images/Booki/bookiAct.webp'
-
-import printItImg from '../images/PrintIt/printit.webp'
-import printItImg2 from '../images/PrintIt/printitRoller.webp'
 
 import sophieBluelImg from '../images/SophieBluel/sophiebluel.webp'
 import sophieBluelImg2 from '../images/SophieBluel/sophiebluelLogin.webp'
@@ -79,13 +78,27 @@ let PeopleGroup = [<FontAwesomeIcon icon={faPeopleGroup} size='2xl'/>, "Gestion"
 
 export const works = [
     {
+        "name": "Pira Multisports",
+        "image": piraimg,
+        "imagesRoller": [piraimg,piraimg2,piraimg3,piraimg4],
+        "date": "Septembre 2025",
+        "link": "https://www.piramultisports.fr",
+        "id": "piraMultisports",
+        "code": [NextJS,ReactJS,MongoDB,NodeJS,Palette,ArrowUpWideShort,PeopleGroup],
+        "category": "Vitrine",
+        "description": "Site d'entreprise pour un coach sportif sur le secteur du Havre.<br/>Mise en place d'un dashboard personnalisé et réutilisable pour la rédaction d'article et la gestion des contenus.<br/>Module d'actualités, d'article, de contact et d'affichage d'univers et de réseaux sociaux.",
+        "fullDescription": 'Pira Multisports est un projet de site vitrine pour un coach sportif basé au Havre.<br/><br/>L’objectif principal de ce site est de présenter les différentes prestations proposées par le coach, mais aussi de permettre une gestion simplifiée des contenus via un dashboard administrateur personnalisé.<br/><br/>Ce dashboard permet au coach de rédiger et publier des articles, gérer les images et vidéos associées, ainsi que de mettre à jour les informations sur les services offerts.<br/><br/>Le site a été conçu avec une attention particulière portée à l’expérience utilisateur, garantissant une navigation fluide et intuitive pour les visiteurs.<br/><br/>',
+        "homePage": true,
+        "isOnline": true
+    },
+    {
         "name": "Valérie Boutique",
         "image": valerieNextImg,
         "imagesRoller": [valerieNextImg,valerieNextImg2,valerieNextImg3,valerieNextImg4,valerieNextImg5,valerieNextImg6,valerieNextImg7,valerieNextImg8,valerieNextImg9,valerieNextImg10,valerieNextImg11,valerieNextImg12],
         "date": "Mars 2024",
         "link": "https://www.valerieboutique.fr",
         "id": "valerieNext",
-        "code": [Html5,Sass,Js,ReactJS,NextJS,MongoDB,NodeJS,Palette,ArrowUpWideShort,PeopleGroup],
+        "code": [NextJS,ReactJS,MongoDB,NodeJS,Palette,ArrowUpWideShort,PeopleGroup],
         "category": "Vitrine",
         "description": "Site vitrine d’une boutique de prêt-à-porter spécialisée dans le Lin et le Velours à Honfleur, Calvados<br/><br/><u><b>Travail réalisé :</b></u> Design, création (cahier des charges, HTML, Sass, NextJS dont React et Node, MongoDB), refonte totale du site, implémentation Back-End de fonctions administratives, hébergement / mise en ligne, présentation à l’entrepreneur",
         "fullDescription": 'Valérie Boutique peut être décrit comme le projet pionnier de CwS. De nombreuses versions ont été réalisées afin de satisfaire chaque envies de cette entrepreneuse basée à Honfleur. Du design à la gestion des nouveaux articles, CwS s’occupe de tout.<br/><br/>Plusieurs étapes de conception ont été nécessaires, notamment de nombreuses phases de tests et de recherche de design pour faire suivre les évolutions créatives et techniques de CwS, incluant l’intégration du framework NextJS.<br/><br/>Dans l’optique d’un déploiement optimal, le site a subi une mise-à-jour importante récemment afin de garantir un référencement naturel renforcé, mais aussi proposer des fonctionnalités majeures tel que la gestion et création d’articles par l’entrepreneuse via par une interface administrateur et l’intégration d’une base donnée, d’un serveur et d’une API qui fonctionnent en cohésion pour fournir aux utilisateurs une expérience de navigation optimale.<br/><br/>Les sites ne restant pas bien longtemps dans une version fixe, des nouveautés techniques aux nouvelles tendances de social engineering, en passant par les découvertes de failles de sécurité, ces besoins requièrent une veille conséquente, ainsi rien ne doit être laissé au hasard. C’est pourquoi le site Valérie Boutique est géré, hébergé, sécurisé, optimisé et mis à jour par mes soins.<br/>Des mises à jours hebdomadaires sont déployés afin de répondre au mieux aux volontés de la boutique, mais également aux besoins des clients.<br/><br/>',
@@ -131,7 +144,7 @@ export const works = [
         "category": "Formation",
         "description": "Création de l'interface d'un site de réservation de restaurant<br/><br/><u><b>Travail réalisé :</b></u> Suivi des designs imposés, utilisation d'animations, création en HTML et Sass, création manuelle de page pour les restaurants",
         "fullDescription": "Kasa est le premier projet React que j’ai eu à coder pendant ma formation.<br/><br/>Ce site internet a nécessité la mise en place d’un environnement de travail particulier, aidé notamment de Create React App.<br/>C’est un site complètement modulaire grâce à ses différents composants créer pour faciliter la mise en place du site web mais aussi du développement.<br/>Il intègre notamment React Router et permet une navigation aisée sur le site et des pages dédiées à chaque articles proposés.<br/><br/>Ce site se rapproche beaucoup de ce qui peut être fait pour votre projet en terme de fonctionnalités et de finitions. Ces technologies ont d’ailleurs été utilisées pour créer le site de CwS et permet de donner un aspect professionnel et pratique à votre site web.<br/><br/>",
-        "homePage": true,
+        "homePage": false,
         "isOnline": false
     },
     {
@@ -187,7 +200,7 @@ export const works = [
         "category": "Portfolio",
         "description": "Création d'un site prototype avant livraison du site final<br/><br/><u><b>Travail réalisé :</b></u> Création de la charte graphique, création en HTML, Sass, JavaScript, mise en ligne, hébergement, communication, référencement",
         "fullDescription": "Le site web de CwS a été le phare de la création de l’entreprise. <br/>D’un côté, il fallait un design marquant et propre à l’esprit de l’entreprise, d’un autre il fallait présenter au mieux et de manière concise mes compétences et mes projets.<br/><br/>Ainsi est né le site CwS. Cette première version était en fait une version test avant la transition vers un site plus complet et plus représentatif de mes capacités.<br/>Dans l’optique où ce site était voué à évoluer fréquemment, il a été pensé pour être complémentent optimisé pour la migration vers ReactJS, mais aussi comme étant un bon exemple de prototype avant la livraison du site final.<br/><br/>Ce site a été l’occasion de découvrir un nouveau pant du design de site internet, de la gestion de projet, d’optimisation, de communication, d'hébergement et de mise en ligne.<br/><br/>",
-        "homePage": true,
+        "homePage": false,
         "isOnline": true
     },
     {
@@ -201,21 +214,7 @@ export const works = [
         "category": "Formation",
         "description": "Projet HTML CSS chez OpenClassrooms, intégrations des designs imposés",
         "fullDescription": "Booki est le premier projet digne de ce nom en HTML et CSS que j’ai pu coder pendant ma formation chez OpenClassrooms.<br/><br/>Gestion de l’armature du site, expérimentation approfondie du CSS, gestion de keyframes, de media queries. <br/>En d’autres termes une création de A à Z du site internet afin de suivre les demandes graphiques avec un code CSS fonctionnel, suivi de sa structure HTML cohérente, mais également techniques au niveau des animations, de l’accessibilité sur téléphone et tablette.<br/><br/>",
-        "homePage": false,
-        "isOnline": true
-    },
-    {
-        "name": "OCR - Print It",
-        "image": printItImg,
-        "imagesRoller": [printItImg,printItImg2],
-        "date": "Décembre 2023",
-        "link": "https://hugeau.github.io/OCR-P5-Print-It/",
-        "id": "printIt",
-        "code": [Html5,Css3,Js],
-        "category": "Formation",
-        "description": "Intégration d'un image roller à un portfolio",
-        "fullDescription": "Print It est un projet d’introduction au JavaScript.<br/><br/>Print It est un site d’entreprise d’impression sur lequel j’ai eu à créer un roller pour les images en JavaScript. Il s’agit d’un des plus petits projets que j’ai eu à faire durant ma formation.<br/><br/>",
-        "homePage": false,
+        "homePage": true,
         "isOnline": true
     },
     {
@@ -229,20 +228,6 @@ export const works = [
         "category": "Formation",
         "description": "Projet de gestion",
         "fullDescription": "Menu Maker est un projet de gestion.<br/><br/>La gestion de projet est une partie très importante du métier de développeur, notamment freelance, elle consiste notamment à la planification des sessions de code, la veille informationnelle, les choix technologiques et leurs présentation à l’entrepreneur.<br/>Cette partie du projet est surement la plus importante dans la vie de votre site internet car elle permet de fixer entre le développeur et le client les besoins et envies, mais surtout d’atteindre un objectif commun qu’est le rendu final du site web.<br/><br/>",
-        "homePage": false,
-        "isOnline": true
-    },
-    {
-        "name": "OCR - Riding Cities",
-        "image": ridingCitiesImg,
-        "imagesRoller": [ridingCitiesImg,ridingCitiesImg2],
-        "date": "Octobre 2023",
-        "link": "https://hugeau.github.io/OCR-Projet2-RidingCities/",
-        "id": "ridingCities",
-        "code": [Html5,Css3],
-        "category": "Formation",
-        "description": "Premier projet HTML CSS chez OpenClassrooms, intégrations de features mineurs",
-        "fullDescription": "Premier projet de programmation de ma formation Intégrateur Web chez OpenClassrooms, Riding cities m’a permis de me plonger dans le grand bain.<br/><br/>Avec ce projet, j’ai commencé à apercevoir le monde du développement web professionnel. <br/>Initiation aux animations CSS, apprentissages des conventions sémantiques du HTML, ce projet était pour moi l’occasion de mettre officiellement un pied dans le monde de la création de sites web.<br/><br/>",
         "homePage": false,
         "isOnline": true
     }
